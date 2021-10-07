a54ec999db1252bcdfeea9cf1d870eb5da7a3b0a
farleyz11 <farley_lopezosorio@hotmail.com>
2021-10-07 16:13:39 -0500

    Se Crea Connexion de Estaciones con BD

--- ProyectoCiclo3.App.Frontend/Pages/Estaciones/List.cshtml.cs
+++ ProyectoCiclo3.App.Frontend/Pages/Estaciones/List.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ProyectoCiclo3.App.Persistencia.AppRepositorios;
 using ProyectoCiclo3.App.Dominio;
- 
+
 namespace ProyectoCiclo3.App.Frontend.Pages
 {
     public class ListEstacionModel : PageModel
@@ -34,7 +34,7 @@ public IActionResult OnPost()
     {
         if(Estacion.id>0)
         {
-        Estacion = repositorioEstaciones.Delete(Estacion.id);
+        repositorioEstaciones.Delete(Estacion.id);
         }
         return RedirectToPage("./List");
     }
